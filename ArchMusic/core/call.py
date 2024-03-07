2ff5d3da9537d5c80aa2478a32f6adc3331b0c36
legendaditya03 <147244657+legendaditya03@users.noreply.github.com>
2024-03-07 17:07:37 +0530

    Update call.py

--- ArchMusic/core/call.py
+++ ArchMusic/core/call.py
@@ -3,7 +3,7 @@
 #
 # This file is part of < https://github.com/legendaditya03/Pusu-Music > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/legendaditya03/ArchMusic/blob/master/LICENSE >
+# Please see < https://github.com/legendaditya03/Pusu-Music/blob/master/LICENSE >
 #
 # All rights reserved.
 #
