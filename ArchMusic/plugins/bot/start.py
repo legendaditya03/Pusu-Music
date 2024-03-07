25c6b390fe57117574b95c37099dd79bf8142865
legendaditya03 <147244657+legendaditya03@users.noreply.github.com>
2024-03-07 17:30:28 +0530

    Update start.py

--- ArchMusic/plugins/bot/start.py
+++ ArchMusic/plugins/bot/start.py
@@ -256,7 +256,7 @@ async def welcome(client, message: Message):
                 userbot = await get_assistant(message.chat.id)
                 out = start_pannel(_)
 
-                video_url = "https://telegra.ph/file/acfb445238b05315f0013.mp4"  # Replace with the actual URL of the video
+                video_url = "https://telegra.ph/file/c2f03eb136e5a4aa663cc.mp4"  # Replace with the actual URL of the video
                 video_caption = _["start_3"].format(config.MUSIC_BOT_NAME, userbot.username, userbot.id)
 
                 await app.send_video(
